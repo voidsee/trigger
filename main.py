d4ae95ff32a4015a0063f32f72662085b34e9aa5
张看 <39406474+voidsee@users.noreply.github.com>
2019-05-01 13:12:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from nmap_t2 import *
-from gen_ip_ports import *
-
-file_name = input("input file name:")
-n = int(input("input number"))
-ips_ports = gen_ports(get_ip_list(file_name), n)
-
-print(ips_ports)
+#!/usr/bin/python3
